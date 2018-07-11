f983ef73e0971818ff29639910e4487287e2de17
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-11 11:28:35 -0400

    Instantiate a user with name and role

--- bin/run.rb
+++ bin/run.rb
@@ -1,15 +1,27 @@
-require_relative 'config/environment'
+require_relative '../config/environment'
+require "pry"
 
-#initialize new user and puts out a welcome/intro message
 def welcome_user
-  puts "Welcome to the Dinner Party! Please enter your name."
+  puts "Welcome to the Dinner Party!"
 end
 
-get_user_name #prompt user for name input, gets name from user
-update_user_name # sets @name == user input
-get_user_role #prompt user to select one of X roles, gets role from user and accepts only X inputs
-invalid_input #puts out error message
-update_user_role # sets @role == user input
-tutorial #explains acceptable input, how the gameplay works, lays out the ultimate goal
-enter room #initialize room with items, character
-describe_room #puts out room, character and items info
+def create_new_user
+  user_instance = User.create(name: nil, role: nil)
+  user_instance.name = user_instance.get_user_name
+  user_instance.role = user_instance.get_user_role
+  user_instance.save
+end
+
+
+welcome_user
+create_new_user
+
+
+# get_user_name #prompt user for name input, gets name from user
+# update_user_name # sets @name == user input
+# get_user_role #prompt user to select one of X roles, gets role from user and accepts only X inputs
+# invalid_input #puts out error message
+# update_user_role # sets @role == user input
+# tutorial #explains acceptable input, how the gameplay works, lays out the ultimate goal
+# enter room #initialize room with items, character
+# describe_room #puts out room, character and items info
--- lib/user.rb
+++ lib/user.rb
@@ -5,19 +5,17 @@ has_many :rooms, through: :items
 
 #initialize new user and puts out a welcome/intro message
 
-  def welcome_user
-    puts "Welcome to the Dinner Party! Please enter your name."
+  def get_user_name
+    puts "Please enter your name."
+    input = gets.chomp
+    self.name = input
   end
 
-
-  get_user_name #prompt user for name input, gets name from user
-  update_user_name # sets @name == user input
-  get_user_role #prompt user to select one of X roles, gets role from user and accepts only X inputs
-  invalid_input #puts out error message
-  update_user_role # sets @role == user input
-  tutorial #explains acceptable input, how the gameplay works, lays out the ultimate goal
-  enter room #initialize room with items, character
-  describe_room #puts out room, character and items info
+  def get_user_role
+    prompt = TTY::Prompt.new
+    input = prompt.select("Select a role for your player:", %w(Sleuth Trouble Influencer Bystander Globetrekker))
+    self.role = input
+  end
 
 
 end
