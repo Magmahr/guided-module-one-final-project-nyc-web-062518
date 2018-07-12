0b17dacb12f2e66d6945025306a344651420f0fd
github username <github email address>
2018-07-12 11:38:37 -0400

    it works

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,8 @@ require "pry"
 
 ActiveRecord::Base.logger.level = 1
 
+ITEM_ARRAY = []
+
 def welcome_user
   puts "Welcome to...
   "
@@ -69,11 +71,9 @@ def describe_a_room
   puts "You find yourself in a #{Room.random_room} with the following items:"
 end
 
-ITEM_ARRAY = []
-
 def pick_an_item
   prompt = TTY::Prompt.new
-  picked_item = prompt.select("Please pick one item:", Item.list_of_random_items)
+  prompt.select("Please pick one item:", Item.list_of_random_items)
 end
 
 def delete_chosen_item(item)
@@ -85,23 +85,17 @@ def delete_chosen_item(item)
   end
 end
 
-def winning_array
-  if ITEM_ARRAY.length == 3
-  # chosen_item = pick_an_item
-  #   # binding.pry
-  # if chosen_item == Item.all.sample.name
-    puts "Congrats, you've picked the lock on the window with the fountain pen, jammed it open with the stock pot and scaled down with the boa!
-    You escaped!"
-  end
-end
-
 def compare_items
   chosen_item = pick_an_item
   if chosen_item == "Fountain Pen" || chosen_item == "Stock Pot" || chosen_item == "Feather Boa"
-  ITEM_ARRAY << chosen_item
+    ITEM_ARRAY << chosen_item
+    delete_chosen_item(chosen_item)
+  else
+    delete_chosen_item(chosen_item)
   end
   if
-    winning_array
+    winning_array == true
+    abort
   else
     if chosen_item == "Fountain Pen" || chosen_item == "Stock Pot" || chosen_item == "Feather Boa"
       puts "Hmm, this might just come in handy..."
@@ -111,7 +105,6 @@ def compare_items
       explore_another_room
     end
   end
-  delete_chosen_item(chosen_item)
 end
 
 def explore_another_room
@@ -120,6 +113,17 @@ def explore_another_room
   compare_items
 end
 # def delete item from database that you just selected
+
+def winning_array
+  if ITEM_ARRAY.length == 3
+  # chosen_item = pick_an_item
+  #   # binding.pry
+  # if chosen_item == Item.all.sample.name
+    puts "Congrats, you've picked the lock on the window with the fountain pen, jammed it open with the stock pot and scaled down with the boa!
+    You escaped!"
+    true
+  end
+end
 
 def play_the_game
   welcome_user
